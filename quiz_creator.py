c80c108a8f266c052f425961e09fde269c2afc63
Leonard Mark Galivo <182197106+leonardmarkgalivo@users.noreply.github.com>
2025-04-07 21:36:13 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,14 @@
+#BEGIN
+
+#Open a text file  in append mode to store quiz questions
+
+#Repeat the following steps until the user chooses to stop:
+    #1. Ask the user to type in a quiz question
+    #2. Ask the user to enter four possible answer choices (a,b,c,d):
+    #3. Ask the user to specify the correct answer by entering either 'a', 'b', 'c', or 'd'
+    #4. Write the question and the four choices to the text file in a clear format
+    #5. Ask the user if they want to add another question
+    #6. If the user answers "yes", repeat the process, if the user answers anything else, stop the loop
+    #7. Display a message saying the quiz has been saved and the program has ended
+
+#END
