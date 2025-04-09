372179c77f8d023ea75dbe2dbe555dd532fe8700
Leonard Mark Galivo <182197106+leonardmarkgalivo@users.noreply.github.com>
2025-04-09 21:12:28 +0800

    initialize_project_structure

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,14 +1,9 @@
-#BEGIN
+import tkinter as tk
 
-#Open a text file  in append mode to store quiz questions
+#Main window
+window = tk.Tk()
+window.title("Quiz Creator")
+window.geometry("500x500")
 
-#Repeat the following steps until the user chooses to stop:
-    #1. Ask the user to type in a quiz question
-    #2. Ask the user to enter four possible answer choices (a,b,c,d):
-    #3. Ask the user to specify the correct answer by entering either 'a', 'b', 'c', or 'd'
-    #4. Write the question and the four choices to the text file in a clear format
-    #5. Ask the user if they want to add another question
-    #6. If the user answers "yes", repeat the process, if the user answers anything else, stop the loop
-    #7. Display a message saying the quiz has been saved and the program has ended
-
-#END
+#Loop
+window.mainloop()
